7573a8a368cf7c2b2d608abd50d2885b5be682c0
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-10 14:37:39 +0300

    Database Schema

---
+++ database-script-0x01/schema.sql
@@ -0,0 +1,65 @@
+CREATE TABLE IF NOT EXISTS User(
+    user_id INT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    Phone_number VARCHAR(20),
+    role VARCHAR(50) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS Property(
+    property_id INT PRIMARY KEY,
+    host_id INT NOT NULL,
+    name VARCHAR NOT NULL,
+    description TEXT NOT NULL,
+    location VARCHAR NOT NULL,
+    pricepernight DECIMAL NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    FOREIGN KEY(host_id) REFERENCES User(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Booking (
+    booking_id INT PRIMARY KEY,
+    property_id INT NOT NULL,
+    user_id INT NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    total_price DECIMAL(10, 2) NOT NULL,
+    status ENUM('pending', 'confirmed', 'canceled') NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id)
+);
+
+CREATE TABLE IF NOT EXISTS Payment (
+    payment_id INT PRIMARY KEY,
+    booking_id INT NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    payment_method ENUM('credit_card', 'paypal', 'stripe') NOT NULL,
+    FOREIGN KEY (booking_id) REFERENCES Booking(booking_id)
+);
+
+CREATE TABLE IF NOT EXISTS Review (
+    review_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    property_id INT NOT NULL,
+    rating INT CHECK (rating >= 1 AND rating <= 5),
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (property_id) REFERENCES Property(property_id)
+);
+
+CREATE TABLE IF NOT EXISTS Message (
+    message_id INT PRIMARY KEY,
+    sender_id INT NOT NULL,
+    receiver_id INT NOT NULL,
+    message_body TEXT NOT NULL,
+    sent_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (sender_id) REFERENCES User(user_id),
+    FOREIGN KEY (receiver_id) REFERENCES User(user_id)
+);
