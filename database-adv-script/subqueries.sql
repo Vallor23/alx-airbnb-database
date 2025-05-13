7ca765fee7f8489d5cc63c4ea16dba86c69fd890
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-13 19:10:39 +0300

    Updating queries

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,6 +1,6 @@
 SELECT *
 FROM Property
-WHERE property_id IN (SELECT property_id FROM Rating GROUP BY property_id HAVING AVG(rating) > 4);
+WHERE property_id IN (SELECT property_id FROM Rating GROUP BY property_id HAVING AVG(rating) > 4.0);
  
 SELECT u.first_name, u.user_id
 FROM user u
