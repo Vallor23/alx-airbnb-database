c99e9ab21cd35768ef0ee355aed1cee5532b5efa
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-13 19:07:26 +0300

    Writing susbqueries

---
+++ database-adv-script/subqueries.sql
@@ -0,0 +1,8 @@
+SELECT *
+FROM Property
+WHERE property_id IN (SELECT property_id FROM Rating GROUP BY property_id HAVING AVG(rating) > 4);
+ 
+SELECT u.first_name, u.user_id
+FROM user u
+WHERE (SELECT COUNT(*) FROM Booking b WHERE b.user_id = u.user_id) > 3
+;
