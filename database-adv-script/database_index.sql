9c5745f452ad81d87914e8de3dea6d0b7c53ceb5
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-16 07:26:25 +0300

    Measure SQL query performance before & after adding indexes

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -14,3 +14,15 @@ CREATE INDEX idx_booking_status ON Booking(status );
 CREATE INDEX  idx_user_property ON(Booking(user_id, property_id));
 
 CREATE INDEX  idx_booking_property ON(Rating(user_id, property_id));
+
+-- MEASURE QUERY PERFOMANCE
+-- Measure Query Perfomance Without Index
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE booking_id = 124;
+
+-- Create an Index
+CREATE INDEX idx_booking_id ON Booking(booking_id);
+
+-- Test the same query after indexing:
+EXPLAIN ANALYZE
+SELECT * FROM Booking WHERE booking_id = 124;
