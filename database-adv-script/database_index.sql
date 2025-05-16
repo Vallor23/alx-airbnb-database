1aedb62e17ebc29a2c5b99c98b40d2775de46cd4
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-16 07:11:29 +0300

    Add indexing of filter properties

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -6,7 +6,11 @@ CREATE INDEX idx_user_id ON Review(user_id);
 
 CREATE INDEX idx_property_id ON Rating(user_id);
 
+CREATE INDEX idx_user_registration_date ON User(registration_date );
+
+CREATE INDEX idx_booking_status ON Booking(status );
+
 -- composite Index/Multiple columns
 CREATE INDEX  idx_user_property ON(Booking(user_id, property_id));
 
-CREATE INDEX  idx_user_property ON(Rating(user_id, property_id));
+CREATE INDEX  idx_booking_property ON(Rating(user_id, property_id));
