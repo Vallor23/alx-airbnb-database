bdb7f3cb6acdd56a478c92d49b6b78670386458b
Vallor23 <enosnyabinda2017@gmail.com>
2025-05-18 20:12:26 +0300

    Implement table partitioning to optimize queries on large datasets.

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,13 @@
-EXPLAIN SELECT * FROM booking b JOIN user u ON b.user_id = u.user_id;
+EXPLAIN SELECT * FROM   Booking b JOIN User u ON b.user_id = u.user_id;
 
-EXPLAIN SELECT * FROM Booking JOIN Property ON Booking.property_id = Property.property_id;
+EXPLAIN SELECT * FROM Booking WHERE(booking_id=2 AND property_id= 1) ;
 
 EXPLAIN SELECT * FROM Booking JOIN Payment  ON Booking.booking_id = Payment.booking_id;
+
+EXPLAIN  SELECT * FROM Booking ORDER BY start_date DESC;
+
+CREATE INDEX idx_user_id ON Booking(user_id);
+
+CREATE INDEX idx_booking_property_id ON Booking(property_id);
+
+CREATE INDEX idx_booking_start_date ON Booking(start_date DESC);
